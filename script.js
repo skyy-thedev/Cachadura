bf0d5c379ffae33d535820257356569f3cd53dfd
skyy_dev <skyzeduardo@gmail.com>
2023-10-01 13:09:36 -0300

    render fixed

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-DOM';
 // Criação de componente
 class UnderAge extends React.Component{
     //Message
-    msg(age){
+    msg(age){   
         alert('Are you oldest to 18?');
         console.log(this);
     }
     render(){
         return <button>Sim</button> ,<button>Não</button>
-        
     }
 };
+ReactDOM.render(<UnderAge />, document.getElementById('root'));
